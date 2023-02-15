f403f9b042ecf6d4f24990d73df5a411cf17fb54
Roy Hashimoto <roy@shoestringresearch.com>
2023-02-14 17:53:10 -0800

    Add GitHub link to worker-modules.

--- worker-modules/index.js
+++ worker-modules/index.js
@@ -1,11 +1,13 @@
 window.addEventListener('DOMContentLoaded', () => {
   document.getElementById('module').addEventListener('click', () => {
+    // Start a ES6 module Worker. The Worker posts a message back.
     const worker = new Worker('./worker.js', { type: 'module' });
     worker.addEventListener('message', ({ data }) => {
       logToOutput(`module: ${data}`);
     });
   });
   document.getElementById('classic').addEventListener('click', () => {
+    // Start a non-module Worker. The Worker posts a message back.
     const worker = new Worker('./worker.js', { type: 'classic' });
     worker.addEventListener('message', ({ data }) => {
       logToOutput(`classic: ${data}`);
@@ -19,5 +21,4 @@ function logToOutput(data) {
   entry.textContent = `${new Date().toLocaleTimeString()} ${data}`;
   output.appendChild(entry);
 }
-
 logToOutput('Click button and Worker will log below...');
