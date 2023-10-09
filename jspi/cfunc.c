2c5742ee59f503d1e89497775e79ebb90fc6420b
Roy Hashimoto <roy@shoestringresearch.com>
2023-10-08 19:44:14 -0700

    Progress but not working.

--- jspi/cfunc.c
+++ jspi/cfunc.c
@@ -1,8 +1,10 @@
 #include <emscripten.h>
+#include <stdio.h>
 
 extern int jfunc(int x);
 
 EMSCRIPTEN_KEEPALIVE
 int cfunc(int x) {
+  printf("cfunc called with %d\n", x);
   return jfunc(x);
 }
--- jspi/libfoo.js
+++ jspi/libfoo.js
@@ -1,12 +1,20 @@
 addToLibrary({
-  $foo_support__postset: 'foo_support',
+  $foo_support__postset: 'foo_support()',
   $foo_support: function() {
 
     _jfunc = function(x) {
+      console.log('jfunc called with', x);
       return Promise.resolve(x * x);
     }
+
+    // This signature specification works, but is it correct?
+    _jfunc.sig = 'ii';
+    _jfunc.isAsync = true;
   },
 
   jfunc: function(){},
-  jfunc__deps: ['$foo_support']
+  jfunc__deps: ['$foo_support'],
+
+  // This signature specification does not work.
+  jfunc__sig: 'ii'
 });
