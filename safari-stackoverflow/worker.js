016efc33305493ce3e1ad5526af6c6f2d14ce7b0
Roy Hashimoto <roy@shoestringresearch.com>
2025-01-24 14:47:16 -0800

    Simplify stack overflow test.

--- safari-stackoverflow/worker.js
+++ safari-stackoverflow/worker.js
@@ -6,6 +6,15 @@ const DB_FILENAME = 'hello.db';
 const ITERATIONS = 50_000;
 const REPORT_INTERVAL = 1000;
 
+const SETUP = `
+  PRAGMA journal_mode=OFF;
+  PRAGMA locking_mode=EXCLUSIVE;
+  PRAGMA synchronous=OFF;
+  CREATE TABLE IF NOT EXISTS hello (id PRIMARY KEY, value TEXT);
+`;
+
+const QUERY = `INSERT OR REPLACE INTO hello VALUES (1234, 'foo')`;
+
 Promise.resolve().then(async () => {
   try {
     postMessage('Worker starting');
@@ -18,37 +27,30 @@ Promise.resolve().then(async () => {
     sqlite3.vfs_register(vfs, true);
     const db = await sqlite3.open_v2(DB_FILENAME);
 
-    // This just makes the test faster.
-    await sqlite3.exec(db, 'PRAGMA journal_mode=OFF');
-    await sqlite3.exec(db, 'PRAGMA locking_mode=EXCLUSIVE');
-    await sqlite3.exec(db, 'PRAGMA synchronous=OFF');
-
-    // Prepare the statement.
-    await sqlite3.exec(db, 'CREATE TABLE IF NOT EXISTS hello (id PRIMARY KEY, value TEXT)');
-    const prepared = await (async function() {
-      for await (const stmt of sqlite3.statements(db, 'INSERT OR REPLACE INTO hello VALUES (?, ?)', { unscoped: true})) {
-        return stmt;
-      }
-    })();
+    postMessage('Set up database');
+    await sqlite3.exec(db, SETUP);
 
     postMessage(`Execute ${ITERATIONS} queries...`);
-    for (let i = 0; i < ITERATIONS; i++) {
-      sqlite3.reset(prepared);
-      sqlite3.bind_int(prepared, 1, i % 100);
-      sqlite3.bind_text(prepared, 2, Math.random());
-      while (await sqlite3.step(prepared) === SQLite.ROW) {
-      }
+    for await (const prepared of sqlite3.statements(db, QUERY)) {
+      for (let i = 0; i < ITERATIONS; i++) {
+        sqlite3.reset(prepared);
+        while (await sqlite3.step(prepared) === SQLite.ROW) {
+          console.assert(false, 'unexpected');
+        }
 
-      if ((i + 1) % REPORT_INTERVAL === 0) {
-        postMessage(`${i + 1} queries`);
+        if ((i + 1) % REPORT_INTERVAL === 0) {
+          postMessage(`${i + 1} queries`);
+        }
+      }
+      if (ITERATIONS % REPORT_INTERVAL !== 0) {
+        postMessage(`${ITERATIONS} queries`);
       }
     }
-    if (ITERATIONS % REPORT_INTERVAL !== 0) {
-      postMessage(`${ITERATIONS} queries`);
-    }
-
+    
     postMessage('Done');
   } catch (e) {
     postMessage(`Error: ${e.message}`);
+    console.error(e);
+    throw e;
   }
 });
